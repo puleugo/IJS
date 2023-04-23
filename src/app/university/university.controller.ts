beb7c4ac9f878a07e0a8ecb4dbe1e11a7b2731fc
puleugo <puleugo@gmail.com>
2023-04-24 00:58:22 +0900

    feat: API SERVICE 연동

--- src/app/university/university.controller.ts
+++ src/app/university/university.controller.ts
@@ -1,8 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { UniversityService } from './university.service';
 import { UniversityCalendarResponse } from '@app/university/dto/university-calendar.response';
 import { UniversityFinishDateProfileResponse } from '@app/university/command/university-finish-date-profile-response.command';
-import { UniversityNearBusResponse } from '@app/university/dto/university-near-bus.response';
 import { UniversityNoticeProfileResponse } from '@app/university/dto/university-notice-profile.response';
 import { UniversityProgramProfileResponse } from '@app/university/dto/university-program-profile.response';
 import { UniversityMealInfoProfileResponse } from '@app/university/dto/university-meal-info-profile.response';
@@ -12,38 +11,56 @@ import { UniversityBusResponse } from '@app/university/dto/university-bus.respon
 export class UniversityController {
   constructor(private readonly universityService: UniversityService) {}
 
-  @Get()
-  async getUniversityMealInfo(): Promise<UniversityMealInfoProfileResponse[]> {
-    return;
+  @Get('meals')
+  async getTodayUniversityMealInfo(): Promise<
+    UniversityMealInfoProfileResponse[]
+  > {
+    const meals = await this.universityService.getUniversityMealInfoByDate(
+      new Date(),
+    );
+    return meals.map((meal) => new UniversityMealInfoProfileResponse(meal));
   }
 
-  @Get()
+  @Get('programs')
   async getUniversityPrograms(): Promise<UniversityProgramProfileResponse[]> {
-    return;
+    const programs = await this.universityService.getUniversityPrograms();
+    return programs.map(
+      (program) => new UniversityProgramProfileResponse(program),
+    );
   }
 
-  @Get()
-  async getUniversityNotices(): Promise<UniversityNoticeProfileResponse[]> {
-    return;
+  @Get('notices/:majorSlug')
+  async getUniversityNotices(
+    @Param('majorSlug') slug: string,
+  ): Promise<UniversityNoticeProfileResponse[]> {
+    const notices = await this.universityService.getUniversityNotices({
+      slug,
+    });
+    return notices.map((notice) => new UniversityNoticeProfileResponse(notice));
   }
 
-  @Get()
-  async getUniversityNearBusInfo(): Promise<UniversityNearBusResponse[]> {
-    return;
-  }
+  // @Get()
+  // async getUniversityNearBusInfo(): Promise<UniversityNearBusResponse[]> {
+  //   const busInfos = await this.universityService.getUniversityNearBusInfo();
+  //   return busInfos.map((busInfo) => new UniversityNearBusResponse(busInfo));
+  // }
 
   @Get()
   async getUniversityFinishDate(): Promise<UniversityFinishDateProfileResponse> {
-    return;
+    const finishDate = await this.universityService.getUniversityFinishDate();
+    return new UniversityFinishDateProfileResponse(finishDate);
   }
 
-  @Get()
+  @Get('calendars')
   async getUniversityCalendarInfo(): Promise<UniversityCalendarResponse> {
-    return;
+    const calendarInfo =
+      await this.universityService.getUniversityCalendarInfo();
+    return new UniversityCalendarResponse(calendarInfo);
   }
 
-  @Get()
+  @Get('bus-info')
   async getUniversityBusInfo(): Promise<UniversityBusResponse> {
-    return;
+    const busInfo = await this.universityService.getUniversityBusInfo();
+    return new UniversityBusResponse(busInfo);
   }
 }
--- src/app/university/university.service.ts
+++ src/app/university/university.service.ts
@@ -1,4 +1,47 @@
 import { Injectable } from '@nestjs/common';
+import { UniversityMealInfoProfileResponseCommand } from '@app/university/command/university-meal-info-profile-response.command';
+import { UniversityProgramProfileResponseCommand } from '@app/university/command/university-program-profile-response.command';
+import { UniversityNoticeProfileResponseCommand } from '@app/university/command/university-notice-profile-response.command';
+import { UniversityNearBusResponseCommand } from '@app/university/command/university-near-bus-response.command';
+import { UniversityFinishDateProfileResponseCommand } from '@app/university/command/university-finished-date-profile-response.command';
+import { UniversityCalendarResponseCommand } from '@app/university/command/university-calendar-response.command';
+import { UniversityBusResponseCommand } from '@app/university/command/university-bus-response.command';
 
 @Injectable()
-export class UniversityService {}
+export class UniversityService {
+  async getUniversityMealInfoByDate(
+    date: Date,
+  ): Promise<UniversityMealInfoProfileResponseCommand[]> {
+    return;
+  }
+
+  async getUniversityPrograms(): Promise<
+    UniversityProgramProfileResponseCommand[]
+  > {
+    return;
+  }
+
+  async getUniversityNotices(data: {
+    slug: string;
+  }): Promise<UniversityNoticeProfileResponseCommand[]> {
+    return;
+  }
+
+  async getUniversityNearBusInfo(): Promise<
+    UniversityNearBusResponseCommand[]
+  > {
+    return;
+  }
+
+  async getUniversityFinishDate(): Promise<UniversityFinishDateProfileResponseCommand> {
+    return;
+  }
+
+  async getUniversityCalendarInfo(): Promise<UniversityCalendarResponseCommand> {
+    return;
+  }
+
+  async getUniversityBusInfo(): Promise<UniversityBusResponseCommand> {
+    return;
+  }
+}
