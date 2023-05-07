675b87cecf6f54980eb8f7abe28493fecf04a658
puleugo <puleugo@gmail.com>
2023-05-08 00:18:15 +0900

    docs: database 연결 정보 .env.example 파일 추가

--- src/data-source.ts
+++ src/data-source.ts
@@ -9,7 +9,7 @@ export const dataSourceConfig: DataSourceOptions = {
   host: process.env.DB_HOST || '',
   port: parseInt(process.env.DB_PORT) || 5432,
   database: process.env.DB_DATABASE || 'ijs_server',
-  username: process.env.DB_USERNAME || 'ijs',
+  username: process.env.DB_USERNAME || 'ijs_user',
   password: process.env.DB_PASSWORD || 'secret',
   entities: ['dist/**/*.entity.js'],
   migrations: ['dist/migrations/**/*.js'],
