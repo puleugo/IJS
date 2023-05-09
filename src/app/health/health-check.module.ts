d7dac301895214c9aea96622abdac4e363fb6eb4
puleugo <puleugo@gmail.com>
2023-05-09 15:23:57 +0900

    feat: 레디스 헬스체크 기능 구현

--- src/app/health/health-check.module.ts
+++ src/app/health/health-check.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
 import { HealthCheckController } from '@app/health/health-check.controller';
+import { RedisModule } from '@nestjs-modules/ioredis';
 
 @Module({
+  imports: [
+    RedisModule.forRootAsync({
+      useFactory: () => ({
+        config: {
+          url: 'redis://localhost:6379',
+        },
+      }),
+    }),
+  ],
   controllers: [HealthCheckController],
 })
 export class HealthCheckModule {}
--- src/main.module.ts
+++ src/main.module.ts
@@ -4,9 +4,20 @@ import { AppModule } from '@app/app.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceConfig } from './data-source';
 import { ConfigModule } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [
+    ClientsModule.register([
+      {
+        name: 'MATH_SERVICE',
+        transport: Transport.REDIS,
+        options: {
+          host: 'localhost',
+          port: 6379,
+        },
+      },
+    ]),
     ConfigModule.forRoot({ isGlobal: true, cache: true }),
     InfrastructureModule,
     AppModule,
