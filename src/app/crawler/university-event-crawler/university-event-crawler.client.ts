61c7cf13222462e781188d5571165aa327d30012
puleugo <puleugo@gmail.com>
2023-05-19 17:58:12 +0900

    feat: 학기 추가 기능 개발

--- src/app/crawler/university-event-crawler/university-event-crawler.client.ts
+++ src/app/crawler/university-event-crawler/university-event-crawler.client.ts
@@ -1,23 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { CrawlerClient } from '@infrastructure/utils/crawler.client';
-import { Repository } from 'typeorm';
+import { Between, LessThan, Like, MoreThan, Repository } from 'typeorm';
 import { UniversityEvent } from '@domain/university/university-event.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import * as puppeteer from 'puppeteer';
 import { removeEscapeCharacters } from '@infrastructure/utils/remove-escape-characters';
+import { UniversitySemester } from '@domain/university/university-semester.entity';
 
 @Injectable()
 export class UniversityEventCrawlerClient implements CrawlerClient {
   constructor(
     @InjectRepository(UniversityEvent)
     private readonly universityEventRepository: Repository<UniversityEvent>,
+    @InjectRepository(UniversitySemester)
+    private readonly universitySemesterRepository: Repository<UniversitySemester>,
   ) {}
 
   async crawl(): Promise<void> {
     const url =
       'https://www.inje.ac.kr/kor/Template/Bsub_page.asp?Ltype=4&Ltype2=1&Ltype3=0&Tname=S_Schedule&Ldir=board/S_Schedule&Lpage=Tboard_L&d1n=4&d2n=2&d3n=1&d4n=1';
     const browser = await puppeteer.launch({
-      headless: true,
+      headless: 'new',
       waitForInitialPage: true,
     });
     const page = await browser.newPage();
@@ -54,6 +57,14 @@ export class UniversityEventCrawlerClient implements CrawlerClient {
       }
     }
     await browser.close();
+
+    const searchYear = new Date().getFullYear();
+    const semesters = await Promise.all([
+      this.getSemesterByUniversityEvents(searchYear, 1),
+      this.getSemesterByUniversityEvents(searchYear, 2),
+    ]);
+    await this.universitySemesterRepository.save(semesters);
+
     return;
   }
 
@@ -61,7 +72,7 @@ export class UniversityEventCrawlerClient implements CrawlerClient {
     return;
   }
 
-  async getDatesByEventKey(
+  private async getDatesByEventKey(
     key: string,
     currentYear: number,
   ): Promise<[startAt: Date, endAt: Date]> {
@@ -80,8 +91,63 @@ export class UniversityEventCrawlerClient implements CrawlerClient {
     return [startAt, endAt];
   }
 
-  async getDatesByEventDate(date: string, currentYear: number): Promise<Date> {
+  private async getDatesByEventDate(
+    date: string,
+    currentYear: number,
+  ): Promise<Date> {
     const [month, day] = date.split('/').map(Number);
     return new Date(currentYear, month - 1, day);
+  }
+
+  private async getSemesterByUniversityEvents(
+    searchYear: number,
+    semesterNumber: number,
+  ): Promise<UniversitySemester> {
+    const { startAt: startedAt } = await this.universityEventRepository.findOne(
+      {
+        where: {
+          title: Like(`%${semesterNumber}학기 학기개시일`),
+          startAt: MoreThan(new Date(searchYear, 0, 1)),
+        },
+      },
+    );
+    let vacation: string;
+    if (semesterNumber === 1) {
+      vacation = '하계방학';
+    } else if (semesterNumber === 2) {
+      vacation = '동계방학';
+    }
+    const { endAt: endedAt } = await this.universityEventRepository.findOne({
+      where: {
+        title: Like(`${vacation} 시작일`),
+        endAt: LessThan(new Date(searchYear, 11, 30)),
+      },
+    });
+
+    const { startAt: middleExamAt } =
+      await this.universityEventRepository.findOne({
+        where: {
+          title: Like('%중간고사 기간'),
+          startAt: Between(startedAt, endedAt),
+        },
+      });
+
+    const { startAt: finalExamAt } =
+      await this.universityEventRepository.findOne({
+        where: {
+          title: Like('%기말고사 기간'),
+          startAt: Between(startedAt, endedAt),
+        },
+      });
+    const year = new Date(startedAt).getFullYear();
+    return this.universitySemesterRepository.create({
+      name: year.toString() + `-${semesterNumber}학기`,
+      year,
+      startedAt,
+      endedAt,
+      semesterNumber,
+      middleExamAt,
+      finalExamAt,
+    });
   }
 }
--- src/app/crawler/university-event-crawler/university-event-crawler.module.ts
+++ src/app/crawler/university-event-crawler/university-event-crawler.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UniversityEventCrawlerClient } from '@app/crawler/university-event-crawler/university-event-crawler.client';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UniversityEvent } from '@domain/university/university-event.entity';
+import { UniversitySemester } from '@domain/university/university-semester.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UniversityEvent])],
+  imports: [TypeOrmModule.forFeature([UniversityEvent, UniversitySemester])],
   providers: [UniversityEventCrawlerClient],
 })
 export class UniversityEventCrawlerModule {}
