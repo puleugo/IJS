ae3f42a1aef58a3bbed720b04e44b4c2d9dc4108
puleugo <puleugo@gmail.com>
2023-05-19 18:27:14 +0900

    feat: 학부 테이블 추가

--- src/domain/university/university-major.entity.ts
+++ src/domain/university/university-major.entity.ts
@@ -1,7 +1,15 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { UniversityMajorNotice } from './university-major-notice.entity';
 import { UniversityEvent } from './university-event.entity';
 import { IUniversityMajor } from './university-major.interface';
+import { UniversityDepartment } from '@domain/university/university-department.entity';
 
 @Entity('university_majors')
 export class UniversityMajor implements IUniversityMajor {
@@ -14,8 +22,15 @@ export class UniversityMajor implements IUniversityMajor {
   @Column('varchar', { length: 10, unique: true })
   slug: string;
 
+  @Column('int')
+  departmentId: number;
+
   @Column('text', { unique: true })
   noticeUrl: string;
+
+  @ManyToOne(() => UniversityDepartment, (department) => department.majors)
+  @JoinColumn({ name: 'department_id', referencedColumnName: 'id' })
+  department: UniversityDepartment;
 
   @OneToMany(() => UniversityMajorNotice, (notice) => notice.major)
   notices: UniversityMajorNotice[];
