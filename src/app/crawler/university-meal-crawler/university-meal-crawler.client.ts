07c4601a077eb7e3b90dc1a6160245c62437d159
puleugo <puleugo@gmail.com>
2023-05-30 12:17:17 +0900

    feat: Redis 분리

--- src/app/crawler/university-meal-crawler/university-meal-crawler.client.ts
+++ src/app/crawler/university-meal-crawler/university-meal-crawler.client.ts
@@ -21,72 +21,76 @@ export class UniversityMealCrawlerClient implements CrawlerClient {
     const url =
       'https://www.inje.ac.kr/kor/Template/Bsub_page.asp?Ltype=5&Ltype2=3&Ltype3=3&Tname=S_Food&Ldir=board/S_Food&Lpage=s_food_view&d1n=5&d2n=4&d3n=4&d4n=0';
     const browser = await puppeteer.launch({
-      headless: 'new',
+      // headless: 'new',
+      headless: false,
       waitForInitialPage: true,
     });
+    try {
+      const page = await browser.newPage();
+      await page.setRequestInterception(true);
+      page.on('request', (req) => {
+        if (
+          req.resourceType() === 'image' ||
+          req.resourceType() === 'font' ||
+          req.resourceType() === 'stylesheet' ||
+          req.resourceType() === 'script' ||
+          req.resourceType() === 'stylesheet' ||
+          req.resourceType() === 'media'
+        ) {
+          req.abort();
+        } else {
+          req.continue();
+        }
+      });
+      await page.goto(url, { waitUntil: 'networkidle2' });
 
-    const page = await browser.newPage();
-    await page.setRequestInterception(true);
-    page.on('request', (req) => {
-      if (
-        req.resourceType() === 'image' ||
-        req.resourceType() === 'font' ||
-        req.resourceType() === 'stylesheet' ||
-        req.resourceType() === 'script' ||
-        req.resourceType() === 'stylesheet' ||
-        req.resourceType() === 'media'
-      ) {
-        req.abort();
-      } else {
-        req.continue();
+      const weekDay = getLastMondayByDate(new Date());
+
+      const meals: UniversityMeal[] = [];
+      for (let i = 3; i < 8; i++) {
+        const elem1 = await page.$(
+          `#table1 > tbody > tr:nth-child(1) > td:nth-child(${i})`,
+        );
+
+        const courseA = await page.evaluate((elem) => elem.textContent, elem1);
+        meals.push(
+          await this.universityMealRepository.create({
+            menu: courseA.split(','),
+            course: MealCourseEnum.A,
+            publishedAt: new Date(weekDay.valueOf()),
+          }),
+        );
+
+        const elem2 = await page.$(
+          `#table1 > tbody > tr:nth-child(2) > td:nth-child(${i})`,
+        );
+        const courseB = await page.evaluate((elem) => elem.textContent, elem2);
+
+        meals.push(
+          await this.universityMealRepository.create({
+            menu: courseB.split(','),
+            course: MealCourseEnum.B,
+            publishedAt: new Date(weekDay.valueOf()),
+          }),
+        );
+
+        const elem3 = await page.$(
+          `#table1 > tbody > tr:nth-child(3) > td:nth-child(${i})`,
+        );
+        const courseC = await page.evaluate((elem) => elem.textContent, elem3);
+        meals.push(
+          await this.universityMealRepository.create({
+            menu: courseC.split(','),
+            course: MealCourseEnum.C,
+            publishedAt: new Date(weekDay.valueOf()),
+          }),
+        );
+        weekDay.setDate(weekDay.getDate() + 1);
       }
-    });
-    await page.goto(url, { waitUntil: 'networkidle2' });
-
-    const weekDay = getLastMondayByDate(new Date());
-
-    const meals: UniversityMeal[] = [];
-    for (let i = 3; i < 8; i++) {
-      const elem1 = await page.$(
-        `#table1 > tbody > tr:nth-child(1) > td:nth-child(${i})`,
-      );
-
-      const courseA = await page.evaluate((elem) => elem.textContent, elem1);
-      meals.push(
-        await this.universityMealRepository.create({
-          menu: courseA.split(','),
-          course: MealCourseEnum.A,
-          publishedAt: new Date(weekDay.valueOf()),
-        }),
-      );
-
-      const elem2 = await page.$(
-        `#table1 > tbody > tr:nth-child(2) > td:nth-child(${i})`,
-      );
-      const courseB = await page.evaluate((elem) => elem.textContent, elem2);
-
-      meals.push(
-        await this.universityMealRepository.create({
-          menu: courseB.split(','),
-          course: MealCourseEnum.B,
-          publishedAt: new Date(weekDay.valueOf()),
-        }),
-      );
-
-      const elem3 = await page.$(
-        `#table1 > tbody > tr:nth-child(3) > td:nth-child(${i})`,
-      );
-      const courseC = await page.evaluate((elem) => elem.textContent, elem3);
-      meals.push(
-        await this.universityMealRepository.create({
-          menu: courseC.split(','),
-          course: MealCourseEnum.C,
-          publishedAt: new Date(weekDay.valueOf()),
-        }),
-      );
-      weekDay.setDate(weekDay.getDate() + 1);
+      await this.universityMealRepository.save(meals);
+    } catch (e) {
+      console.log(e);
     }
-    await this.universityMealRepository.save(meals);
     await browser.close();
     return;
   }
