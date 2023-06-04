795f799cdb39494b7a0897750662871fa9af638c
puleugo <puleugo@gmail.com>
2023-06-04 15:07:06 +0900

    fix: 일요일에는 차주의 학식이 조회되던 버그 수정

--- src/infrastructure/utils/get-last-monday-by-date.ts
+++ src/infrastructure/utils/get-last-monday-by-date.ts
@@ -1,3 +1,4 @@
 export function getLastMondayByDate(date: Date) {
+  if (date.getDay() === 0) new Date(date.setDate(date.getDate() - 6));
   return new Date(date.setDate(date.getDate() - date.getDay() + 1));
 }
