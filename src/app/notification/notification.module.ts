0a6f71a2000753283326a2cbfab55fe456a9ac32
puleugo <puleugo@gmail.com>
2023-08-26 22:42:55 +0900

    refactor: UtilModule 글로벌 적용

--- src/app/notification/notification.module.ts
+++ src/app/notification/notification.module.ts
@@ -3,11 +3,10 @@ import { NotificationService, } from '@app/notification/notification.service';
 import { TypeOrmModule, } from '@nestjs/typeorm';
 import { Notification, } from '@domain/user/notification/notification.entity';
 import { NotificationToken, } from '@domain/user/notification/notification-token.entity';
-import { LoggerService, } from '@infrastructure/utils/logger.service';
 
 @Module({
 	imports: [TypeOrmModule.forFeature([Notification, NotificationToken,]),],
-	providers: [NotificationService, LoggerService,],
+	providers: [NotificationService,],
 	exports: [NotificationService,],
 })
 export class NotificationModule {
